2ead72cb1fdf9c62a504543125db4a32d7f4317e
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-01-07 04:49:00 -0800

    update defaults

--- geoip_demo.py
+++ geoip_demo.py
@@ -20,9 +20,9 @@ def get_lux():
     return get_brightness(latitude_deg, longitude_deg, date)
 
 
-def get_brightness(latitude_deg=42.206,
-                   longitude_deg=-71.382,
-                   date=datetime.datetime(2007, 2, 18, 2, 13, 1, 130320, tzinfo=datetime.timezone.utc)):
+def get_brightness(latitude_deg=37.70577,  # daly city
+                   longitude_deg=-122.46192,
+                   date=datetime.datetime(2020, 1, 6, 2, 13, 1, 0, tzinfo=datetime.timezone.utc)):
     return radiation.get_radiation_direct(date, get_altitude(latitude_deg, longitude_deg, date))
 
 
