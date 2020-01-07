c08352cfb27c83c6b023d3777ad6091e6cb0a244
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-01-07 05:02:49 -0800

    more comment

--- geoip_demo.py
+++ geoip_demo.py
@@ -1,6 +1,7 @@
 # pip install geoip2 pysolar
 import datetime
 import urllib.request
+from typing import Tuple
 
 import geoip2.database
 from pysolar import radiation
@@ -9,7 +10,7 @@ from pysolar.solar import get_altitude
 
 # from phue import Bridge
 
-def get_lux():
+def get_location_from_ip() -> Tuple[float, float]:
     reader = geoip2.database.Reader('GeoLite2-City.mmdb')
     ips = urllib.request.urlopen('https://checkip.amazonaws.com').read().decode('ascii').split(
         ', ')  # can return multiple ips
@@ -17,16 +18,22 @@ def get_lux():
     response = reader.city(main_ip)
     latitude_deg = response.location.latitude
     longitude_deg = response.location.longitude
+    return latitude_deg, longitude_deg
+
+
+def get_lux() -> float:
+    latitude_deg, longitude_deg = get_location_from_ip()
     date = datetime.datetime.now(tz=datetime.timezone.utc)
     return get_brightness(latitude_deg, longitude_deg, date)
 
 
 def get_brightness(latitude_deg=37.70577,  # daly city
                    longitude_deg=-122.46192,
-                   date=datetime.datetime(2020, 1, 6, 2, 13, 1, 0, tzinfo=datetime.timezone.utc)):
+                   date=datetime.datetime(2020, 1, 6, 2, 13, 1, 0, tzinfo=datetime.timezone.utc)) -> float:
     return radiation.get_radiation_direct(date, get_altitude(latitude_deg, longitude_deg, date))
 
 
 brightness = get_lux()
+print(brightness)
 # group = Bridge().groups[1]
 # group.brightness = brightness
