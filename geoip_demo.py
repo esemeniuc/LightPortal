0cd94c9d9b9f5b90f874e22f99441d673b1c227a
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-01-27 07:27:23 -0800

    work on geoip

--- geoip_demo.py
+++ geoip_demo.py
@@ -1,9 +1,11 @@
-# pip install geoip2 pysolar
+# pip install geoip2 pysolar pytz timezonefinder
 import datetime
 import urllib.request
 from typing import Tuple
 
 import geoip2.database
+import pytz
+import timezonefinder
 from pysolar import radiation
 from pysolar.solar import get_altitude
 
@@ -21,19 +23,30 @@ def get_location_from_ip() -> Tuple[float, float]:
     return latitude_deg, longitude_deg
 
 
-def get_lux() -> float:
-    latitude_deg, longitude_deg = get_location_from_ip()
-    date = datetime.datetime.now(tz=datetime.timezone.utc)
-    return get_brightness(latitude_deg, longitude_deg, date)
+def get_time_with_timezone(latitude_deg: float, longitude_deg: float) -> datetime.datetime:
+    tf = timezonefinder.TimezoneFinder()
+    tz_str = tf.timezone_at(lat=latitude_deg, lng=longitude_deg)
+    tz = pytz.timezone(tz_str)
+    date = datetime.datetime.now(tz=tz)
+    return date
 
 
-def get_brightness(latitude_deg=37.70577,  # daly city
-                   longitude_deg=-122.46192,
-                   date=datetime.datetime(2020, 1, 6, 2, 13, 1, 0, tzinfo=datetime.timezone.utc)) -> float:
+# outputs the brightness of a given lat lon
+def get_lux(latitude_deg: float, longitude_deg: float, date: datetime) -> float:
     return radiation.get_radiation_direct(date, get_altitude(latitude_deg, longitude_deg, date))
 
 
-brightness = get_lux()
+def demo():
+    # daly city
+    latitude_deg = 37.70577
+    longitude_deg = -122.46192
+    date = datetime.datetime(2020, 1, 27, 7, 13, 1, 0, tzinfo=datetime.timezone.utc)
+    return get_lux(latitude_deg, longitude_deg, date)
+
+
+latitude_deg, longitude_deg = get_location_from_ip()
+date = get_time_with_timezone(latitude_deg, longitude_deg)
+brightness = get_lux(latitude_deg, longitude_deg, date)
 print(brightness)
 # group = Bridge().groups[1]
 # group.brightness = brightness
