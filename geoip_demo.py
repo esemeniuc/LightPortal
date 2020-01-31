cf5f580be5e59f1cce34ea608d60053d66c41d10
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-01-30 17:51:42 -0800

    more geoip work

--- geoip_demo.py
+++ geoip_demo.py
@@ -1,16 +1,21 @@
-# pip install geoip2 pysolar pytz timezonefinder
+# pip install geoip2 pysolar pytz timezonefinder numpy
 import datetime
 import urllib.request
 from typing import Tuple
 
 import geoip2.database
+import numpy as np
 import pytz
 import timezonefinder
 from pysolar import radiation
 from pysolar.solar import get_altitude
+from pysolar.util import get_sunrise_sunset
+
+import kelvin_rgb_conversion
 
 
 # from phue import Bridge
+
 
 def get_location_from_ip() -> Tuple[float, float]:
     reader = geoip2.database.Reader('GeoLite2-City.mmdb')
@@ -31,22 +36,89 @@ def get_time_with_timezone(latitude_deg: float, longitude_deg: float) -> datetim
     return date
 
 
-# outputs the brightness of a given lat lon
-def get_lux(latitude_deg: float, longitude_deg: float, date: datetime) -> float:
-    return radiation.get_radiation_direct(date, get_altitude(latitude_deg, longitude_deg, date))
+# outputs the raw lux brightness of a given lat lon, and a normalized 0-255 value
+def get_lux(latitude_deg: float, longitude_deg: float, date: datetime) -> Tuple[float, int]:
+    raw_lux = radiation.get_radiation_direct(date, get_altitude(latitude_deg, longitude_deg, date))
+    sr, ss = get_sunrise_sunset(latitude_deg, longitude_deg, date)
+    print('sunrise: ', sr)
+    print('sunset: ', ss)
+    daylight_span = ss - sr
+    midday = sr + daylight_span / 2
+    max_lux = radiation.get_radiation_direct(date, get_altitude(latitude_deg, longitude_deg, midday))
+    print('max_lux: ', max_lux)
+    normalized = (raw_lux / max_lux) * 255  # put in 0 - 255
+    return raw_lux, np.round(np.clip(normalized, 0, 255))
+
+
+# x: sun altitude, from 0 deg to 180 deg
+# returns sunlight color temp in kelvin (k)
+# assumes the following:
+# SUNRISE_TEMP = 3000
+# MIDDAY_TEMP = 5500
+# SUNSET_TEMP = 3000
+# see https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
+def colour_temp(alt_deg: float) -> int:
+    if not 0 < alt_deg < 180:
+        return 0
+
+    if alt_deg > 90:
+        alt_deg -= 90  # sunrise and sunset are mirrors of each other
+
+    if alt_deg < 30:
+        # linear function based on
+        # https://micro.magnet.fsu.edu/primer/lightandcolor/colortemperatureintro.html
+        return int(50 * alt_deg + 3000)
+
+    if alt_deg < 90:
+        return int(5 / 18 * (alt_deg - 90) ** 2 + 5500)
 
 
 def demo():
     # daly city
     latitude_deg = 37.70577
     longitude_deg = -122.46192
-    date = datetime.datetime(2020, 1, 27, 7, 13, 1, 0, tzinfo=datetime.timezone.utc)
+    date = datetime.datetime(2020, 1, 27, 7, 58, 1, 0, tzinfo=datetime.timezone.utc)
     return get_lux(latitude_deg, longitude_deg, date)
 
 
-latitude_deg, longitude_deg = get_location_from_ip()
-date = get_time_with_timezone(latitude_deg, longitude_deg)
-brightness = get_lux(latitude_deg, longitude_deg, date)
-print(brightness)
-# group = Bridge().groups[1]
-# group.brightness = brightness
+# k: color temp (3000 to 5500k)
+# brightness: 0-255
+def update(brightness: int, k: int) -> None:
+    # philips hue stuff
+    # group = Bridge().groups[1]
+    # def from_k(k):
+    #     return (1, 1)
+    #
+    # h, s = from_k(k)
+    # group.hue = h
+    # group.saturation = s
+    # group.brightness = brightness
+
+    # grid stuff
+    from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+    options = RGBMatrixOptions()
+    options.rows = 32
+    options.chain_length = 1
+    options.parallel = 1
+    options.hardware_mapping = 'adafruit-hat'
+    matrix = RGBMatrix(options=options)
+    matrix.brightness = brightness
+    r, g, b = kelvin_rgb_conversion.color_temp_to_rgb(k)
+    matrix.Fill(r, g, b)
+
+
+def main():
+    latitude_deg, longitude_deg = get_location_from_ip()
+    date = get_time_with_timezone(latitude_deg, longitude_deg)
+    # date = datetime.datetime(2020, 1, 29, 12, 30, 1, 0, tzinfo=pytz.timezone('America/Los_Angeles'))
+    brightness, normalized_brightness = get_lux(latitude_deg, longitude_deg, date)
+    print('brightness: ', brightness, normalized_brightness)
+
+    # print('transit:', tr)
+    alt = get_altitude(latitude_deg, longitude_deg, date)
+    print('alt :', alt)
+    k = colour_temp(alt)
+    update(normalized_brightness, k)
+
+main()
